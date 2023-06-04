5a9af81277131bbff8598c75890aff6b4d8774f4
ceheller <camiloheller@gmail.com>
2023-06-04 14:49:39 -0300

    adding css and making it responsive

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import emailjs from 'emailjs-com';
-
+import './styles.css'; // Import custom CSS file for styling
 
 const App = () => {
   const [image, setImage] = useState('');
@@ -10,6 +10,7 @@ const App = () => {
   const [isTimeUp, setIsTimeUp] = useState(false);
   const [email, setEmail] = useState('');
   const [isEmailSent, setIsEmailSent] = useState(false);
+  let timer; // Declare the timer variable
 
   useEffect(() => {
     // Generate a random number for the image
@@ -19,7 +20,7 @@ const App = () => {
     setImage(imageUrl);
 
     // Timer countdown
-    const timer = setInterval(() => {
+    timer = setInterval(() => {
       setTimeRemaining((prevTime) => prevTime - 1);
     }, 1000);
 
@@ -42,13 +43,18 @@ const App = () => {
     setEmail(event.target.value);
   };
 
+  const finishTextSooner = () => {
+    clearInterval(timer);
+    setIsTimeUp(true);
+  };
+
   const sendEmail = () => {
     const templateParams = {
       from_email: email,
       to_email: email, // Send the email to the user-provided email address
       userText: text, // Pass the user's text as the template parameter
     };
-  
+
     emailjs
       .send('service_chwt4ld', 'template_wy617zg', templateParams, 'fUdLpu-M23Eyc2Doq')
       .then((response) => {
@@ -61,34 +67,38 @@ const App = () => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      {image && <img src={image} alt="Inspiration" />}
-      <textarea
-        value={text}
-        onChange={handleTextChange}
-        disabled={isTimeUp}
-        style={{ width: '900px', height: '100px', margin: '20px 0' }}
-        placeholder="Write your text here"
-      />
-      {isTimeUp && (
-        <div>
-          <h3>Time's up! Here's your text:</h3>
-          <p>{text}</p>
-          {!isEmailSent && (
-            <div>
-              <p>Please enter your email to receive the text:</p>
-              <input type="email" value={email} onChange={handleEmailChange} />
-              <button onClick={sendEmail}>Send Email</button>
-            </div>
-          )}
-          {isEmailSent && <p>Email sent successfully!</p>}
-        </div>
-      )}
-      {!isTimeUp && (
-        <div>
-          <h3>Time Remaining: {Math.floor(timeRemaining / 60)}:{timeRemaining % 60}</h3>
-        </div>
-      )}
+    <div className="container">
+      <div className="logo">Project62</div> {/* Add the logo */}
+      <div className="content">
+        {image && <img src={image} alt="Inspiration" className="image" />}
+        <textarea
+          value={text}
+          onChange={handleTextChange}
+          disabled={isTimeUp}
+          className="text-area"
+          placeholder="Write your text here"
+        />
+        {isTimeUp && (
+          <div>
+            <h3>Time's up! Here's your text:</h3>
+            <p>{text}</p>
+            {!isEmailSent && (
+              <div>
+                <p>Please enter your email to receive the text:</p>
+                <input type="email" value={email} onChange={handleEmailChange} />
+                <button onClick={sendEmail}>Send Email</button>
+              </div>
+            )}
+            {isEmailSent && <p>Email sent successfully!</p>}
+          </div>
+        )}
+        {!isTimeUp && (
+          <div>
+            <h3>Time Remaining: {Math.floor(timeRemaining / 60)}:{timeRemaining % 60}</h3>
+            <button onClick={finishTextSooner}>Finish text sooner</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
